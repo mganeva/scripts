e8f4136634bd957c80a5bb5d7b9bc968b98a9ad3
Kilian92 <rueckschloss.kilian@web.de>
2014-09-15 09:44:35 +0200

    Files to generate Workspaces with data TOF, two theta and counts (with more comments)

--- dns/dns_classes_loading_data_tof_twoTheta_counts_into_mantid.py
+++ dns/dns_classes_loading_data_tof_twoTheta_counts_into_mantid.py
@@ -4,7 +4,8 @@ import numpy
 
 class input_workspace:
 	
-	"""Holds the path to an Inputfile to create coressponding workspace."""
+	"""Holds the path to an Inputfile to create coressponding workspace and manages actions to do on one 
+		inputfile."""
 	
 	def __init__(self, path_to_input_workspace):
 		self.data_location =  path_to_input_workspace
@@ -47,7 +48,9 @@ class input_workspace:
 		return [twoThetaArr, countsArr]
 
 		
-	def create_a_Arr_couts_vs_two_theta_vs_tof(self):
+	def create_a_Arr_two_theta_vs_tof(self):
+		
+		"""Extract data to create a workspace counts vs tof vs two theta"""
 		
 		#Load NeXus file
 		ws = LoadEventNexus(self.data_location)
@@ -55,6 +58,8 @@ class input_workspace:
 		#Set data to extract the angles
 		samplePos = ws.getInstrument().getSample().getPos()
 		beamDirection = V3D(0,0,1)
+		#choose a smaller number for m other wise the execution of create_a_table_couts_vs_two_theta_vs_tof()
+		#will take lots of time (see econd loop in create_a_table_couts_vs_two_theta_vs_tof)
 		#m = ws.getNumberHistograms()
 		m=1000
 		twoTheta0 = ws.getSampleDetails().getLogData("s2").value
@@ -69,8 +74,7 @@ class input_workspace:
 				twoTheta =  (d.getTwoTheta(samplePos, beamDirection)*180/math.pi) + float(twoTheta0)
 				tof = ws.getEventList(i).getTofs().tolist()
 				for j in range(len(tof)): 
-					event = [twoTheta, tof[j]]
-					#print event 
+					event = [twoTheta, tof[j]] 
 					eventsArr.append(event)
 		
 		ws.delete()
@@ -79,7 +83,8 @@ class input_workspace:
 
 class input:
 	
-	"""Holds array of input_workspaces we want to analyse """
+	"""Holds array of input workspaces we want to analyse and manges the actions to do one a set of
+	inputfiles."""
 	
 	def __init__(self, paths_to_input_workspaces):
 		self.input_workspaces = []
@@ -120,26 +125,33 @@ class input:
 		
 	def create_a_table_couts_vs_two_theta_vs_tof(self):
 		
+		"""Creats a table workspace counts vs tof vs two theta out of the input data"""
 		
+		#Generate the desired array and table
 		eventsArr = []
 		twoTheta_tof_countsArr = CreateEmptyTableWorkspace()
 		twoTheta_tof_countsArr.addColumn("double", "two_theta")
 		twoTheta_tof_countsArr.addColumn("double", "tof")
 		twoTheta_tof_countsArr.addColumn("double", "counts")
 		
+		#Filling up the arrays with the desired values
 		for i in range(len(self.input_workspaces)):
-			eventArr = self.input_workspaces[i].create_a_Arr_couts_vs_two_theta_vs_tof()
+			eventArr = self.input_workspaces[i].create_a_Arr_two_theta_vs_tof()
 			for j in range(len(eventArr)): eventsArr.append(eventArr[j])
-		print eventsArr
 		
+		#Counting events and write data into table workspace
+		#This takes a lot of time 
 		while eventsArr != []:
+			
+			#count envent
 			counts = eventsArr.count(eventsArr[0])
-			twoTheta_tof_count =  [eventsArr[0][0], eventsArr[0][1] ,counts]
-			print twoTheta_tof_count 
-			event = eventsArr[0]
-			while event in eventsArr:
-				eventsArr.remove(event)
+			twoTheta_tof_count =  [eventsArr[0][0], eventsArr[0][1] ,counts] 
 			twoTheta_tof_countsArr.addRow(twoTheta_tof_count)
+			
+			#delete event from eventsArr
+			event = eventsArr[0]
+			while event in eventsArr: eventsArr.remove(event)
+			
 		
 
 #input_ws = input(["/home/kilian92/datafiles/dns/test1_event.nxs", "/home/kilian92/datafiles/dns/test2_event.nxs", "/home/kilian92/datafiles/dns/test3_event.nxs"])
--- dns/dns_get_table_counts_twoTheta_tof.py
+++ dns/dns_get_table_counts_twoTheta_tof.py
@@ -3,21 +3,22 @@ import time
 import numpy
 
 
-def create_a_table_couts_vs_two_theta_vs_tof():
+def create_a_table_couts_vs_two_theta_vs_tof(path):
 		
 		#Load NeXus file
-		ws = LoadEventNexus("/home/kilian92/datafiles/dns/test1_event.nxs")
+		ws = LoadEventNexus(path)
 		
 		#Set data to extract the angles
 		samplePos = ws.getInstrument().getSample().getPos()
 		beamDirection = V3D(0,0,1)
-		m = ws.getNumberHistograms()
-		#m=1000
+		#choose a smaller number for m other wise the execution 
+		#will take lots of time (see second loop)
+		#m = ws.getNumberHistograms()
+		m=1000
 		twoTheta0 = ws.getSampleDetails().getLogData("s2").value
 	
 		#Generate the desired array and table
 		eventsArr = []
-		
 		twoTheta_tof_countsArr = CreateEmptyTableWorkspace()
 		twoTheta_tof_countsArr.addColumn("double", "two_theta")
 		twoTheta_tof_countsArr.addColumn("double", "tof")
@@ -30,21 +31,25 @@ def create_a_table_couts_vs_two_theta_vs_tof():
 				twoTheta =  (d.getTwoTheta(samplePos, beamDirection)*180/math.pi) + float(twoTheta0)
 				tof = ws.getEventList(i).getTofs().tolist()
 				for j in range(len(tof)): 
-					event = [twoTheta, tof[j]]
-					#print event 
+					event = [twoTheta, tof[j]] 
 					eventsArr.append(event)
 		
+		#Counting events and write data into table workspace
 		while eventsArr != []:
+			
+			#count envent
 			counts = eventsArr.count(eventsArr[0])
 			twoTheta_tof_count =  [eventsArr[0][0], eventsArr[0][1] ,counts]
-			print twoTheta_tof_count 
+			twoTheta_tof_countsArr.addRow(twoTheta_tof_count)
+			
+			#delete event from eventsArr
 			event = eventsArr[0]
 			while event in eventsArr:
 				eventsArr.remove(event)
-			twoTheta_tof_countsArr.addRow(twoTheta_tof_count)
+			
 		
 		
 		ws.delete()
 		
 		
-create_a_table_couts_vs_two_theta_vs_tof()
+create_a_table_couts_vs_two_theta_vs_tof("/home/kilian92/datafiles/dns/test1_event.nxs")
